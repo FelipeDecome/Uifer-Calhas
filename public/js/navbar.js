4c458a67067408b6f61f13e29151d9f4a9f3101e
FelipeDecome <felipe.decome@hotmail.com>
2020-01-30 15:32:51 -0300

    Update navbar.js

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,38 +1,48 @@
 const buttons = document.querySelectorAll('[data-target]');
 
 for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', function () {
 
-    //TODO: Organizar a sequencia de ações
-    //! REMOVE TODAS AS CLASSES 'ACTIVE'
-    for (let i = 0; i < buttons.length; i++) {
-      if (buttons[i].classList.contains('active')) {
-        buttons[i].classList.remove('active');
-      }
-    }
-
-    if (!buttons[i].classList.contains('active')) {
-      buttons[i].classList.add('active');
-    }
-
-    let target = buttons[i].getAttribute('data-target');
-    let element = document.querySelector(target);
-
-    if (!element.classList.contains('collapse')) {
-      removeAllCollapses();
-
-      element.classList.add('collapse');
-    } else {
-      element.classList.remove('collapse');
-    }
-  });
+  const collapses = [];
+  collapses.push(buttons[i].getAttribute('data-target'));
 
   //! Pensar melhor nessa parte
   function removeAllCollapses() {
-    let collapses = document.getElementsByClassName('collapse');
-
+    console.log(collapses[i].classList, '1');
     for (let i = 0; i < collapses.length; i++) {
-      collapses[i].classList.remove('collapse');
+      let collapse = document.querySelector(collapses[i]);
+      collapse.classList.remove('collapse');
+      console.log(collapse.classList, '2');
     }
   }
+
+  console.log(collapses, '3');
+
+  buttons[i].addEventListener('click', function () {
+    if (!buttons[i].classList.contains('active')) {
+      //* REMOVE TODAS AS CLASSES 'ACTIVE'
+      for (let i = 0; i < buttons.length; i++) {
+        if (buttons[i].classList.contains('active')) {
+          buttons[i].classList.remove('active');
+        }
+      }
+      //* ADCIONA O ACTIVE NO BOTÃO CLICADO
+      buttons[i].classList.add('active');
+    } else {
+      //* REMOVE O ACTIVE NO BOTÃO CLICADO
+      buttons[i].classList.remove('active');
+    }
+    /*     if (!buttons[i].classList.contains('active')) {
+          buttons[i].classList.add('active');
+        } */
+    let element = document.querySelector(buttons[i].getAttribute('data-target'));
+    console.log(element.classList, '4');
+
+    if (!element.classList.contains('collapse')) {
+      removeAllCollapses();
+      element.classList.add('collapse');
+    } else {
+      removeAllCollapses();
+      element.classList.remove('collapse');
+    }
+  });
 }
