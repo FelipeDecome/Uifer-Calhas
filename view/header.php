d81b145b82a74b6b4d53d040e46a770e544de02a
FelipeDecome <felipe.decome@hotmail.com>
2020-03-05 22:45:39 -0300

    uifer

---
+++ view/header.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="./css/main.css">
+<header>
+    <div class="container row">
+        <div class="logo_wrapper">
+            <i class="logo"> <?php include_once './images/logo.svg';?> </i>
+            <span>Uifer Calhas</span>
+        </div>
+    </div>
+</header>
