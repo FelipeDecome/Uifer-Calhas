82eb2dd50ce9ca7196dc091051b1e5aae2590470
Felipe Decome <felipe.decome@hotmail.com>
2020-03-16 16:10:28 -0300

    about

--- view/about.php
+++ view/about.php
@@ -1,3 +1,22 @@
+<link rel="stylesheet" href="./css/about.css">
 <section id="about">
-    
+    <header class="section__title">
+        <span class="__title-back">Quem Somos?</span>
+        <h2 class="__title">
+            <span>Bem vindo a</span>
+            <span class="__title-great">Uifer Calhas</span>
+        </h2>
+    </header>
+
+    <main class="about">
+        <div class="about__content">
+            <div class="__content-aboutText">
+                <p>A empresa iniciou suas atividades com o Sr. Fernando Decome, e hoje a mesma conta com 30 anos de
+                    experiência e confiança de seus clientes no mercado, sem deixar de lado a tradição familiar do bom
+                    atendimento e eficiência em seus trabalhos.</p>
+                <p>Nós, da Uifer Calhas, somos uma empresa especializada em manutenção, fabricação de calhas e funilaria
+                    industrial.</p>
+            </div>
+        </div>
+    </main>
 </section>
