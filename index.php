ebfdc9d775341d1d560bae256ffa0831c59f15dc
Felipe Decome <felipe.decome@hotmail.com>
2020-03-17 15:03:20 -0300

    search bar

--- index.php
+++ index.php
@@ -17,9 +17,9 @@ require_once __DIR__ . "./config.php";
 
 <body>
 
-
     <?php include_once "./view/pages/header.php";?>
 
+    <script src="https://kit.fontawesome.com/67895c6c28.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- view/components/search-bar/search-bar.php
+++ view/components/search-bar/search-bar.php
@@ -1,9 +1,9 @@
 <link rel="stylesheet" href="<?php echo PATH_COMPONENTS ?>search-bar/css/search-bar.css">
 
-<div class="search-bar" id="searchBar">
+<div class="search-bar flex __j-center __a-center" id="searchBar">
     <div class="__wrapper">
         <input type="text" class="__search-field" required />
-        <span class="__placehoder"> O que está procurando? </span>
-        <!-- <i class="fas fa-search"></i> -->
+        <span class="__placeholder"> O que está procurando? </span>
+        <i class="__icon fas fa-search"></i>
     </div>
 </div>
