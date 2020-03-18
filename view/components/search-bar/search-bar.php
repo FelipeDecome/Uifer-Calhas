f5a9a3780e3c57bcb31c589e8680558fe3d45dd6
FelipeDecome <felipe.decome@hotmail.com>
2020-03-17 22:08:22 -0300

    search-bar almost

--- view/components/search-bar/search-bar.php
+++ view/components/search-bar/search-bar.php
@@ -1,8 +1,9 @@
 <link rel="stylesheet" href="<?php echo PATH_COMPONENTS ?>search-bar/css/search-bar.css">
 
-<div class="search-bar flex __j-center __a-center" id="searchBar">
-    <div class="__wrapper">
+<div class="search-bar flex __j-center __a-center" id="searchBar" data-state="shown">
+    <div class="__wrapper flex __j-between __a-center">
         <input type="text" class="__search-field" required />
+        <span class="__field-border"></span>
         <span class="__placeholder"> O que está procurando? </span>
         <i class="__icon fas fa-search"></i>
     </div>
